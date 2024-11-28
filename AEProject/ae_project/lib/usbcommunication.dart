495644263bb1b6f43e6662c6f216f37130fb2477
Alessandro <alessandro.cammalleri00@gmail.com>
2024-11-28 23:49:35 +0100

    usbCommunication development
    starting to develop usb serial communication

--- AEProject/ae_project/lib/usbcommunication.dart
+++ AEProject/ae_project/lib/usbcommunication.dart
@@ -1,5 +1,10 @@
+import 'dart:ffi';
+import 'dart:typed_data';
+
 import 'package:ae_project/debug_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_libserialport/flutter_libserialport.dart';
+import 'package:number_system/number_system.dart';
 
 import 'main.dart';
 
@@ -9,6 +14,10 @@ class Usbcommunication extends StatefulWidget {
   @override
   State<Usbcommunication> createState() => _UsbCommunication();
 }
+
+List<String> availablePorts = SerialPort.availablePorts;
+//print(availablePorts);
+SerialPort port1 = SerialPort('COM3');
 
 class _UsbCommunication extends State<Usbcommunication> {
   @override
@@ -50,19 +59,35 @@ class _UsbCommunication extends State<Usbcommunication> {
     collectSamples();
     separateSamples_type();
 
-    await Future.delayed(const Duration(seconds: 10));
+    await Future.delayed(const Duration(seconds: 2));
 
     Navigator.of(context)
-      .push(MaterialPageRoute(builder: (BuildContext context) {
+        .push(MaterialPageRoute(builder: (BuildContext context) {
       return DebugPage();
     }));
   }
 
-  void usbinitcomm() {}
+  void usbinitcomm() {
+    try {
+      port1.openReadWrite();
+      final config = SerialPortConfig();
+      config.baudRate = 115200;
+      port1.config = config;
+    } on SerialPortError catch (err, _) {
+      print(SerialPort.lastError);
+    }
+  }
+
   void sendMessage(bool debug) {
     if (debug) {
       //message
+      String message = '#1';
+      print(_byteToUint8List(message.hexToDEC()));
+      port1.write(_byteToUint8List(message.hexToDEC()));
     } else {
+      //message
+      String message = '#2';
+      port1.write(_byteToUint8List(message.hexToDEC()));
       //message
     }
   }
@@ -70,4 +95,17 @@ class _UsbCommunication extends State<Usbcommunication> {
   void collectSamples() {}
   void separateSamples_charge() {}
   void separateSamples_type() {}
+
+  Uint8List _stringToUint8List(String data) {
+    List<int> codeUnits = data.codeUnits;
+    Uint8List uint8list = Uint8List.fromList(codeUnits);
+    return uint8list;
+  }
+
+  Uint8List _byteToUint8List(int data) {
+    List<int> dataunit = [];
+    dataunit.add(data);
+    Uint8List uint8list = Uint8List.fromList(dataunit);
+    return uint8list;
+  }
 }
